fae11edf26428e4ade4dded3fb2a5d8df0c67b53
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-02-16 20:44:09 +0300

    Task 3,4

--- HomeWork/src/HW_1/Task3.java
+++ HomeWork/src/HW_1/Task3.java
@@ -1,4 +1,18 @@
 package HW_1;
 
 public class Task3 {
+    public static void main(String[] args) {
+        int a[] ={1,-2,30};
+        System.out.println("Колличество положительных чисел " + findPositive(a));
+
+    }
+
+    public static int findPositive(int [] a) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]>0) count++;
+        }
+        return count;
+    }
+
 }
---
+++ HomeWork/src/HW_1/Task4.java
@@ -0,0 +1,24 @@
+package HW_1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int a[] ={1,-2,30};
+        System.out.println("Колличество положительных чисел " + findPositive(a)+ '\n'+  "Колличество отрцательных чисел " + findNegative(a));
+
+    }
+
+    public static int findPositive(int [] a) {
+        int countPos = 0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]>0) countPos++;
+        }
+        return countPos;
+    }
+    public static int findNegative(int [] a) {
+        int countNeg = 0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]<0) countNeg++;
+        }
+        return countNeg;
+    }
+}
