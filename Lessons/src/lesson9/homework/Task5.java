a840de41e7830b6ab0a86f94b79f9ac7ac1ddfe0
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-03-21 15:01:33 +0300

    Lesson9 Homework 5,6

--- Lessons/src/lesson9/homework/Task5.java
+++ Lessons/src/lesson9/homework/Task5.java
@@ -3,20 +3,37 @@ package lesson9.homework;
 5.	Даны два предложения посчитать сколько слов совпадает.
  */
 
+import java.util.ArrayList;
+
 public class Task5 {
     public static void main(String[] args) {
-        String s1 = "ada";
-        String s2 = "ada";
+        String s1 = "ada 1 123 3 45 4 67 56";
+        String s2 = "ada 2 123 2 45 2 67 52";
         checkEquals(s1, s2);
 
     }
 
     public static void checkEquals(String s1, String s2) {
-        int count = 0;
+
         if (s1.equals(s2)) {
             System.out.println("Строки идентичны");
         } else {
 
+            String[] s1split= s1.split(" ");
+            String[] s2split= s2.split(" ");
+            boolean[] count = new boolean[s1split.length];
+            for (int i = 0; i < s1split.length; i++) {
+                if (s1split[i].equals(s2split[i])){
+                    count[i]=true;
+                }
+            }
+            int countTrue=0;
+            for (boolean check : count) {
+                if (check) {
+                    countTrue++;
+                }
+            }
+            System.out.println("Колличество слов которые совпадают: " + countTrue );
         }
     }
 }
--- Lessons/src/lesson9/homework/Task6.java
+++ Lessons/src/lesson9/homework/Task6.java
@@ -1,5 +1,6 @@
 package lesson9.homework;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
 
@@ -13,24 +14,87 @@ import java.util.Locale;
  */
 public class Task6 {
     public static void main(String[] args) {
-        String row = "Object-oriented programming is a programming language model organized around objects rather than \"actions\" and data rather than logic. Object-oriented programming blabla. Object-oriented programming bla.";
+        String row = "Object-oriented programming is a programming language model organized around objects rather than \"actions\" and data rather than logic. Object-oriented programming blabla. Object-oriented programming bla. Object-oriented programming blabla. Object-oriented programming blabla. Object-oriented programming bla. Object-oriented programming blabla.";
+        String change = "object-oriented programming";
+        String changeTo="OOP";
         System.out.println(row);
-//        String lower = row.toLowerCase();
+        String lower = row.toLowerCase();
 //        System.out.println(lower);
-        System.out.println(replaceToOOP(row));
+//        System.out.println(replaceToOOP(lower));
+        ind(row, change,changeTo);
     }
 
-    public static String replaceToOOP(String row) {
-        String[] newRow = row.split("object-oriented programming");
+    //    public static String replaceToOOP(String row) {
+//        StringBuilder newRow = new StringBuilder(row);
+////        String[] newRow = row.split("object-oriented programming");
+//        int count = 0;
+//        for (int i = 0; i < newRow.length; i++) {
+//            if (newRow[i].equalsIgnoreCase("object-oriented programming")) {
+//                count++;
+//                if (count % 2 == 0) {
+//                    newRow[i] = "OOP";
+//                }
+//            }
+//        }
+//        return Arrays.toString(newRow);
+//    }
+    public static void ind(String row, String change, String changeTo) {
+        String newRow = row;
+        ArrayList<String> split = new ArrayList<>();
         int count = 0;
-        for (int i = 0; i < newRow.length; i++) {
-            if (newRow[i].equalsIgnoreCase("object-oriented programming")) {
-                count++;
-                if (count % 2 == 0) {
-                    newRow[i] = "OOP";
+        int fIndex = newRow.toLowerCase().indexOf(change);//"object-oriented programming"
+        int lenght = change.length();
+        if (fIndex == -1) {
+            System.out.println("В строке нет совпадеий с " + change);
+        } else {
+            count++;
+            while (fIndex != -1) {
+                fIndex = newRow.toLowerCase().indexOf(change, fIndex + 1);//"object-oriented programming"
+                if (fIndex != -1) {
+                    count++;
+                    if (count%2==0){
+                        split.add(newRow.substring(0,fIndex));
+                        newRow = newRow.substring(fIndex);
+                        split.add(changeTo);
+                        fIndex = newRow.toLowerCase().indexOf(change);
+                        newRow = newRow.substring(fIndex+lenght);
+                    }
+//                split.add(newRow.substring(lIndex,fIndex));
+//                newRow = newRow.substring(fIndex);
+                } else {
+                    break;
                 }
             }
+            split.add(newRow);
         }
-        return Arrays.toString(newRow);
+        for (int i = 0; i < split.toArray().length; i++) {
+        System.out.print(split.get(i));
+        }
     }
 }
+
+//    public static void ind (String row,String change) {
+//        StringBuilder newRow = new StringBuilder(row);
+//        String input = row.toLowerCase();
+//        String search= input;
+//
+//        int fIndex = input.indexOf("object-oriented programming");
+//        int lIndex = fIndex + change.length();
+//        int count = 0;
+//        while (fIndex != -1) {
+//            count++;
+//            fIndex = search.indexOf("object-oriented programming");
+//            if (count % 2 == 0) {
+////                String search2= search;
+////                while (lIndex!=-1){
+////                    search2 = search2.substring(0,lIndex - 1);
+////                    lIndex = search2.lastIndexOf("object-oriented programming");
+////                }
+////                input.spl
+//            search = input.substring(fIndex + 1);
+//            }
+////                newRow.replace(fIndex,lIndex,"OOP");
+//        }
+//        System.out.println("No of *is* in the input is : " + count);
+//    }
+//}
